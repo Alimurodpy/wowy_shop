0d76de5221a38d37eee47169434f2467a0a9261c
Alimurod <alimurodakbarov@gmail.com>
2024-09-08 19:56:38 +0500

    Add file

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from mptt.admin import DraggableMPTTAdmin
+from apps.products.models import Category
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(DraggableMPTTAdmin):
+    mptt_level_indent = 20
+    mptt_indent_field = "title"
+    list_display = ('tree_actions', 'indented_title', 'created_at', 'is_active')
+    list_display_links = ('indented_title',)
+    list_filter = ('is_active', 'created_at')
+    search_fields = ('title',)
+    list_per_page = 20
--- apps/products/models.py
+++ apps/products/models.py
@@ -1,9 +1,42 @@
 from django.db import models
 from apps.base.models import BaseModel
+from mptt.models import MPTTModel, TreeForeignKey
+#import RichTextField
+from ckeditor.fields import RichTextField
 
-class Product(BaseModel):
+class Category(BaseModel, MPTTModel):
     title = models.CharField(max_length=255, verbose_name="Category name")
+    slug = models.SlugField(unique=True, verbose_name="Slug")
     icon = models.TextField(null=True, blank=True, verbose_name="Icon")
-    photo = models.ImageField(upload_to='category_photo/', null=True, blank=True)
+    photo = models.ImageField(upload_to='category_photo/', null=True, blank=True , verbose_name="Category photo")
 
-     
+    parent = TreeForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='children'
+    )
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+    class MPTTMeta:
+        level_attr = 'mptt_level'
+        order_insertion_by=['title']
+    def __str__(self):
+        return self.title
+
+class Banner(BaseModel):
+    title = models.CharField(max_length=255, verbose_name="Banner title")
+    photo = models.ImageField(upload_to='banner_photo/', verbose_name="Banner photo")
+    body = RichTextField()
+    
+
+    class Meta:
+        verbose_name = "Banner"
+        verbose_name_plural = "Banners"
+
+    def __str__(self):
+        return self.title
