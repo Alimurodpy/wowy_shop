dddebaaf8dbf30982e17f8b6a8668c39cf0096b1
Alimurod <alimurodakbarov@gmail.com>
2024-09-08 15:26:38 +0500

    Account app finished_

--- apps/products/models.py
+++ apps/products/models.py
@@ -3,5 +3,6 @@ from apps.base.models import BaseModel
 
 class Product(BaseModel):
     title = models.CharField(max_length=255, verbose_name="Category name")
-    icon = models.TextField(null=True, blank=True)
+    icon = models.TextField(null=True, blank=True verbose_name="Icon")
+    photo = models.ImageField(upload_to='category_photo/', null=True, blank=True)
      
